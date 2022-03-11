9a991b632821cf2d2938d6148fd889616cfd87cd
Calvine Otieno <nyarangaotieno@gmail.com>
2022-03-11 16:10:46 +0300

    chore: add jwt module and passport
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,6 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import * as argon from 'argon2';
 import { error } from 'console';
@@ -6,7 +8,11 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(
+        private prisma: PrismaService, 
+        private jwt: JwtService,
+        private config: ConfigService,
+        ) { }
     async signup(dto: AuthDto) {
         // generate the password hash
         const hash = await argon.hash(dto.password);
@@ -28,7 +34,7 @@ export class AuthService {
             delete user.hash
 
             // return the saved user
-            return user
+            return this.signToken(user.id, user.email)
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) { 
                 if (error.code === 'P2002') { 
@@ -68,7 +74,28 @@ export class AuthService {
         );
 
         // send back the user
-        delete user.hash
-        return user
+        return this.signToken(user.id, user.email)
+    }
+
+    async signToken(
+        userId: number,
+        email: string,
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email,
+        };
+
+        const secret = this.config.get('JWT_SECRET')
+        
+        const token =  await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret,
+        },
+        );
+
+        return {
+            access_token: token,
+        };
     }
 }
